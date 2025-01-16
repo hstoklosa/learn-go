22999a525fb4fcb3bc79800f83cda4b071418dfa
hstoklosa <hubert.stoklosa23@gmail.com>
2025-01-16 10:24:47 +0000

    create runner for the examples

---
+++ 0-learn.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1. Hello World")
+	helloWorld()
+
+	fmt.Println("\n2. Values") 
+	values()
+
+	fmt.Println("\n3. Variables")
+	variables()
+
+	fmt.Println("\n4. Constants")
+	constants()
+
+	fmt.Println("\n5. For")
+	forLoops()
+
+	fmt.Println("\n6. ifElse")
+	ifElse()
+
+    fmt.Println("\n7. Switch")
+	switchCase()
+
+    fmt.Println("\n8. Arrays")
+	arraysDs()
+    
+    fmt.Println("\n9. Slices")
+    slicesDs()
+}
