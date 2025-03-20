dccbc958a399cd1e58556cde2ab3648207f6d164
imexotic <hubert.stoklosa23@gmail.com>
2025-03-20 00:10:35 +0000

    modify comments in functions.go

--- 02-functions.go/functions.go
+++ 02-functions.go/functions.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// https://go.dev/blog/declaration-syntax
+// REF: https://go.dev/blog/declaration-syntax
 func add(x int, y int) int {
 	return x + y
 }
@@ -17,7 +17,7 @@ func swap(x, y string) (string, string) {
 
 /*
  * A return statement without arguments returns the named return values. This is known
- * as a "naked" return. Naked return statements should be used only in short functions,
+ * as a "naked" return. Naked return statements should be used only in short functions.
  */
  func split(sum int) (x, y int) {
 	x = sum * 4 / 9
